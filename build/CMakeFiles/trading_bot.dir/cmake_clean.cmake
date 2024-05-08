918b2489d80adfd9033f087fd5e239be9dc0971a
Siyi Xu <e1300538@u.nus.edu>
2024-05-08 15:33:13 +0800

    update branch

---
+++ build/CMakeFiles/trading_bot.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trading_bot.dir/binance_api.cpp.o"
+  "CMakeFiles/trading_bot.dir/binance_api.cpp.o.d"
+  "CMakeFiles/trading_bot.dir/test/main.cpp.o"
+  "CMakeFiles/trading_bot.dir/test/main.cpp.o.d"
+  "CMakeFiles/trading_bot.dir/test/trade_endpoint.cpp.o"
+  "CMakeFiles/trading_bot.dir/test/trade_endpoint.cpp.o.d"
+  "trading_bot"
+  "trading_bot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trading_bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/trading_bot.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for trading_bot.
+# This may be replaced when dependencies are built.
