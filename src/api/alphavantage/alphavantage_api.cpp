54fc5973e78c25697574357baf95679290a88b10
Michael Kang <51043502+onepersonhere@users.noreply.github.com>
2024-06-22 12:13:41 +0800

    alpha vantage api test

--- src/api/alphavantage/alphavantage_api.cpp
+++ src/api/alphavantage/alphavantage_api.cpp
@@ -3,8 +3,6 @@
 //
 
 #include "alphavantage_api.h"
-// alphavantage_api.cpp
-#include "alphavantage_api.h"
 #include <iostream>
 #include <string>
 
@@ -14,17 +12,209 @@ AlphaVantageAPI::AlphaVantageAPI(const std::string& apiKey)
     std::cout << "API Key: " << apiKey << std::endl;
 }
 
-std::string AlphaVantageAPI::getTimeSeriesDaily(const std::string& symbol) {
-    std::string endpoint = "function=TIME_SERIES_DAILY&symbol=" + symbol + "&apikey=" + apiKey;
+std::string AlphaVantageAPI::buildUrl(const std::string& function, const std::string& params) {
+    return "function=" + function + "&apikey=" + apiKey + "&" + params;
+}
+
+// Core Stock APIs
+std::string AlphaVantageAPI::getIntraday(const std::string& symbol, const std::string& interval) {
+    std::string endpoint = buildUrl("TIME_SERIES_INTRADAY", "symbol=" + symbol + "&interval=" + interval);
     return curlMain.callAPI(endpoint);
 }
 
-std::string AlphaVantageAPI::getTimeSeriesIntraday(const std::string& symbol, const std::string& interval) {
-    std::string endpoint = "function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&interval=" + interval + "&apikey=" + apiKey;
+std::string AlphaVantageAPI::getDaily(const std::string& symbol) {
+    std::string endpoint = buildUrl("TIME_SERIES_DAILY", "symbol=" + symbol);
     return curlMain.callAPI(endpoint);
 }
 
-std::string AlphaVantageAPI::getQuoteEndpoint(const std::string& symbol) {
-    std::string endpoint = "function=GLOBAL_QUOTE&symbol=" + symbol + "&apikey=" + apiKey;
+std::string AlphaVantageAPI::getDailyAdjusted(const std::string& symbol) {
+    std::string endpoint = buildUrl("TIME_SERIES_DAILY_ADJUSTED", "symbol=" + symbol);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getWeekly(const std::string& symbol) {
+    std::string endpoint = buildUrl("TIME_SERIES_WEEKLY", "symbol=" + symbol);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getWeeklyAdjusted(const std::string& symbol) {
+    std::string endpoint = buildUrl("TIME_SERIES_WEEKLY_ADJUSTED", "symbol=" + symbol);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getMonthly(const std::string& symbol) {
+    std::string endpoint = buildUrl("TIME_SERIES_MONTHLY", "symbol=" + symbol);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getMonthlyAdjusted(const std::string& symbol) {
+    std::string endpoint = buildUrl("TIME_SERIES_MONTHLY_ADJUSTED", "symbol=" + symbol);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getQuote(const std::string& symbol) {
+    std::string endpoint = buildUrl("GLOBAL_QUOTE", "symbol=" + symbol);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::searchTicker(const std::string& keywords) {
+    std::string endpoint = buildUrl("SYMBOL_SEARCH", "keywords=" + keywords);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getGlobalMarketStatus() {
+    std::string endpoint = buildUrl("GLOBAL_MARKET_STATUS", "");
+    return curlMain.callAPI(endpoint);
+}
+
+// Options Data APIs
+std::string AlphaVantageAPI::getRealtimeOptions(const std::string& symbol) {
+    std::string endpoint = buildUrl("OPTION_PREMIUM", "symbol=" + symbol);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getHistoricalOptions(const std::string& symbol) {
+    std::string endpoint = buildUrl("OPTION_HISTORICAL", "symbol=" + symbol);
+    return curlMain.callAPI(endpoint);
+}
+
+// Alpha Intelligence™
+std::string AlphaVantageAPI::getNewsSentiments(const std::string& symbol) {
+    std::string endpoint = buildUrl("NEWS_SENTIMENTS", "symbol=" + symbol);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getTopGainersLosers() {
+    std::string endpoint = buildUrl("TOP_GAINERS_LOSERS", "");
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getAnalytics(const std::string& symbol, const std::string& interval) {
+    std::string endpoint = buildUrl("ANALYTICS", "symbol=" + symbol + "&interval=" + interval);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getSlidingWindowAnalytics(const std::string& symbol, const std::string& interval) {
+    std::string endpoint = buildUrl("ANALYTICS_SLIDING_WINDOW", "symbol=" + symbol + "&interval=" + interval);
+    return curlMain.callAPI(endpoint);
+}
+
+// Fundamental Data
+std::string AlphaVantageAPI::getCompanyOverview(const std::string& symbol) {
+    std::string endpoint = buildUrl("OVERVIEW", "symbol=" + symbol);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getDividends(const std::string& symbol) {
+    std::string endpoint = buildUrl("DIVIDEND", "symbol=" + symbol);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getSplits(const std::string& symbol) {
+    std::string endpoint = buildUrl("SPLIT", "symbol=" + symbol);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getIncomeStatement(const std::string& symbol) {
+    std::string endpoint = buildUrl("INCOME_STATEMENT", "symbol=" + symbol);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getBalanceSheet(const std::string& symbol) {
+    std::string endpoint = buildUrl("BALANCE_SHEET", "symbol=" + symbol);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getCashFlow(const std::string& symbol) {
+    std::string endpoint = buildUrl("CASH_FLOW", "symbol=" + symbol);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getEarnings(const std::string& symbol) {
+    std::string endpoint = buildUrl("EARNINGS", "symbol=" + symbol);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getListingDelistingStatus() {
+    std::string endpoint = buildUrl("LISTING_STATUS", "");
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getEarningsCalendar() {
+    std::string endpoint = buildUrl("EARNINGS_CALENDAR", "");
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getIPOCalendar() {
+    std::string endpoint = buildUrl("IPO_CALENDAR", "");
+    return curlMain.callAPI(endpoint);
+}
+
+// Forex (FX)
+std::string AlphaVantageAPI::getForexExchangeRates(const std::string& fromCurrency, const std::string& toCurrency) {
+    std::string endpoint = buildUrl("CURRENCY_EXCHANGE_RATE", "from_currency=" + fromCurrency + "&to_currency=" + toCurrency);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getForexIntraday(const std::string& fromSymbol, const std::string& toSymbol, const std::string& interval) {
+    std::string endpoint = buildUrl("FX_INTRADAY", "from_symbol=" + fromSymbol + "&to_symbol=" + toSymbol + "&interval=" + interval);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getForexDaily(const std::string& fromSymbol, const std::string& toSymbol) {
+    std::string endpoint = buildUrl("FX_DAILY", "from_symbol=" + fromSymbol + "&to_symbol=" + toSymbol);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getForexWeekly(const std::string& fromSymbol, const std::string& toSymbol) {
+    std::string endpoint = buildUrl("FX_WEEKLY", "from_symbol=" + fromSymbol + "&to_symbol=" + toSymbol);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getForexMonthly(const std::string& fromSymbol, const std::string& toSymbol) {
+    std::string endpoint = buildUrl("FX_MONTHLY", "from_symbol=" + fromSymbol + "&to_symbol=" + toSymbol);
+    return curlMain.callAPI(endpoint);
+}
+
+// Cryptocurrencies
+std::string AlphaVantageAPI::getCryptoExchangeRates(const std::string& symbol) {
+    std::string endpoint = buildUrl("CRYPTO_RATING", "symbol=" + symbol);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getCryptoIntraday(const std::string& symbol, const std::string& market, const std::string& interval) {
+    std::string endpoint = buildUrl("CRYPTO_INTRADAY", "symbol=" + symbol + "&market=" + market + "&interval=" + interval);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getCryptoDaily(const std::string& symbol, const std::string& market) {
+    std::string endpoint = buildUrl("CRYPTO_DAILY", "symbol=" + symbol + "&market=" + market);
+    return curlMain.callAPI(endpoint);
+}
+
+std::string AlphaVantageAPI::getCryptoWeekly(const std::string& symbol, const std::string& market) {
+    std::string endpoint = buildUrl("CRYPTO_WEEKLY", "symbol=" + symbol + "&market=" + market);
+    return curlMain.callAPI(endpoint);
+}
+std::string AlphaVantageAPI::getCryptoMonthly(const std::string& symbol, const std::string& market) {
+    std::string endpoint = buildUrl("CRYPTO_MONTHLY", "symbol=" + symbol + "&market=" + market);
+    return curlMain.callAPI(endpoint);
+}
+
+// Commodities
+std::string AlphaVantageAPI::getCommodity(const std::string& symbol) {
+    std::string endpoint = buildUrl("COMMODITY", "symbol=" + symbol);
+    return curlMain.callAPI(endpoint);
+}
+
+// Economic Indicators
+std::string AlphaVantageAPI::getEconomicIndicator(const std::string& function) {
+    std::string endpoint = buildUrl(function, "");
+    return curlMain.callAPI(endpoint);
+}
+
+// Technical Indicators
+std::string AlphaVantageAPI::getTechnicalIndicator(const std::string& symbol, const std::string& indicator, const std::string& interval, const std::string& timePeriod) {
+    std::string endpoint = buildUrl(indicator, "symbol=" + symbol + "&interval=" + interval + "&time_period=" + timePeriod);
     return curlMain.callAPI(endpoint);
 }
--- test/main.cpp
+++ test/main.cpp
@@ -40,9 +40,66 @@ void binanceApiTest() {
 
 void alphavantageApiTest() {
     AlphaVantageAPI api("VX2MEZ75W5JNSLFY");
-    PRINT_RES(api.getTimeSeriesDaily("AAPL"));
-    PRINT_RES(api.getTimeSeriesIntraday("AAPL", "5min"));
-    PRINT_RES(api.getQuoteEndpoint("AAPL"));
+
+    // Core Stock APIs
+    PRINT_RES(api.getIntraday("AAPL", "5min"));
+    PRINT_RES(api.getDaily("AAPL"));
+    PRINT_RES(api.getDailyAdjusted("AAPL"));
+    PRINT_RES(api.getWeekly("AAPL"));
+    PRINT_RES(api.getWeeklyAdjusted("AAPL"));
+    PRINT_RES(api.getMonthly("AAPL"));
+    PRINT_RES(api.getMonthlyAdjusted("AAPL"));
+    PRINT_RES(api.getQuote("AAPL"));
+    PRINT_RES(api.searchTicker("Apple"));
+    PRINT_RES(api.getGlobalMarketStatus());
+
+    // Alpha Intelligence™
+    PRINT_RES(api.getNewsSentiments("AAPL"));
+    PRINT_RES(api.getTopGainersLosers());
+    PRINT_RES(api.getAnalytics("AAPL", "5min"));
+    PRINT_RES(api.getSlidingWindowAnalytics("AAPL", "5min"));
+
+    // Fundamental Data
+    PRINT_RES(api.getCompanyOverview("AAPL"));
+    PRINT_RES(api.getDividends("AAPL"));
+    PRINT_RES(api.getSplits("AAPL"));
+    PRINT_RES(api.getIncomeStatement("AAPL"));
+    PRINT_RES(api.getBalanceSheet("AAPL"));
+    PRINT_RES(api.getCashFlow("AAPL"));
+    PRINT_RES(api.getEarnings("AAPL"));
+    PRINT_RES(api.getListingDelistingStatus());
+    PRINT_RES(api.getEarningsCalendar());
+    PRINT_RES(api.getIPOCalendar());
+
+    // Forex (FX)
+    PRINT_RES(api.getForexExchangeRates("USD", "EUR"));
+    PRINT_RES(api.getForexIntraday("EUR", "USD", "5min"));
+    PRINT_RES(api.getForexDaily("EUR", "USD"));
+    PRINT_RES(api.getForexWeekly("EUR", "USD"));
+    PRINT_RES(api.getForexMonthly("EUR", "USD"));
+
+    // Cryptocurrencies
+    PRINT_RES(api.getCryptoExchangeRates("BTC"));
+    PRINT_RES(api.getCryptoIntraday("BTC", "USD", "5min"));
+    PRINT_RES(api.getCryptoDaily("BTC", "USD"));
+    PRINT_RES(api.getCryptoWeekly("BTC", "USD"));
+    PRINT_RES(api.getCryptoMonthly("BTC", "USD"));
+
+    // Commodities
+    PRINT_RES(api.getCommodity("WTI"));
+    PRINT_RES(api.getCommodity("BRENT"));
+
+    // Economic Indicators
+    PRINT_RES(api.getEconomicIndicator("REAL_GDP"));
+    PRINT_RES(api.getEconomicIndicator("REAL_GDP_PER_CAPITA"));
+    PRINT_RES(api.getEconomicIndicator("TREASURY_YIELD"));
+    PRINT_RES(api.getEconomicIndicator("FEDERAL_FUNDS_RATE"));
+    PRINT_RES(api.getEconomicIndicator("CPI"));
+    PRINT_RES(api.getEconomicIndicator("INFLATION"));
+    PRINT_RES(api.getEconomicIndicator("RETAIL_SALES"));
+    PRINT_RES(api.getEconomicIndicator("DURABLE_GOODS"));
+    PRINT_RES(api.getEconomicIndicator("UNEMPLOYMENT_RATE"));
+    PRINT_RES(api.getEconomicIndicator("NONFARM_PAYROLL"));
 }
 
 void printRes(const std::string& api, const std::string& methodName) {
